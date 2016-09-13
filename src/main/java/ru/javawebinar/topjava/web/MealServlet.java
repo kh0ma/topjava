eb78e2f8056a0b00ad3ea15f7bc0d2533f176d5d
kh0ma <khomenko.dp@gmail.com>
2016-09-13 14:04:37 +0300

    По аналогии с UserServlet добавить MealServlet и mealList.jsp.

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by kh0ma on 13.09.16.
+ */
+public class MealServlet extends HttpServlet{
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to mealList");
+
+//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        response.sendRedirect("mealList.jsp");
+    }
+}
---
+++ src/main/webapp/mealList.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kh0ma
+  Date: 13.09.16
+  Time: 13:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meal list</title>
+</head>
+<body>
+<h2><a href="index.html">Home</a></h2>
+<h2>Meal list</h2>
+</body>
+</html>
